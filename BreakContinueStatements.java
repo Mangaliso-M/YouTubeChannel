5bd662eab5643415396a5c8709b7651b882242de
Mangaliso Mngomezulu <65779966+Mangaliso-M@users.noreply.github.com>
2021-04-13 10:27:22 +0200

    Update BreakContinueStatements.java

--- BreakContinueStatements.java
+++ BreakContinueStatements.java
@@ -3,17 +3,22 @@ package javaIntroduction;
 public class BreakContinueStatements {
 	public static void main(String[] args) {
 		
-		//break, continue
-		
+		//break: when condition is met, stop the execution
 		for(int i = 0 ; i < 5; i++) {
 			
-		
-			if(i == 2) {
-				continue;
-			}
+			if(i == 2)
+				break;
+			
 			System.out.println(i);	
 		}
 		
-		
+		//continue: skip i = 2, i.e i = 0 , 1, 3, , 4 ...
+		for(int i = 0 ; i < 5; i++) {
+			
+			if(i == 2)
+				continue;
+			
+			System.out.println(i);	
+		}
 	}
 }
