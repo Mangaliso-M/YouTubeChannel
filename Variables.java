7b0a5e99790f815cc68682167e3d3c4f7af1282e
Mangaliso Mngomezulu <65779966+Mangaliso-M@users.noreply.github.com>
2021-04-13 09:19:25 +0200

    Updated comments

--- Variables.java
+++ Variables.java
@@ -4,6 +4,14 @@ public class Variables {
 	
 	public static void main(String[] args) {
 		
-	
+	//declaration 
+	//type variableName;
+        
+        //initializing
+	//variableName = value;
+		
+	//declare and initialize Once
+        //type variableName = value;
+		
 	}
 }
