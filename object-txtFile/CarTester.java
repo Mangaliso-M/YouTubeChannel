3517a61633741ef54c06c3a1a2c7c1dee0747eb0
Mangaliso Mngomezulu <65779966+Mangaliso-M@users.noreply.github.com>
2021-10-01 22:30:07 +0200

    Update CarTester.java

--- object-txtFile/CarTester.java
+++ object-txtFile/CarTester.java
@@ -1,4 +1,9 @@
-package gettingBetter;
+/*
+  @Author: Mangaliso Mngomezulu
+  BSc Computer Science and Information Technology Student, University Of KwaZulu Natal South Africa
+  Description: A CarTester class that tests the Driver and Car class by instiating objects and writing the objects to a text file
+*/
+
 import java.io.*;
 import java.util.*;
 
@@ -6,15 +11,19 @@ public class CarTester {
 	
    public static Driver createDriver() {
 	   
+	   //declare variables
 	   String name="";
 	   int age = 0;
 	   Scanner in = new Scanner(System.in);
 	   
+	   //get the input
 	   System.out.print("Driver name: ");  name = in.nextLine();
 	   System.out.print("Driver age : ");  age = in.nextInt();
 	   
+	   //use the input to create the object
 	   Driver driver = new Driver(name, age); //create driver object
 	   
+	   //return the Driver object
 	   return driver;
    } //ends
    
@@ -22,17 +31,20 @@ public class CarTester {
    
    public static Car createCar() {
 	   
+	   //declare variables
 	   Scanner in = new Scanner(System.in);
 	   String carName = "";
 	   int year = 0;
 	   
-	   
+	   //get the user input
 	   System.out.print("Car make: ");  carName = in.nextLine();
 	   System.out.print("Car year: ");  year = in.nextInt();
 	   Driver driver = createDriver();
 	   
+	   //create the Car object
 	   Car carObject = new Car(carName, year, driver);
 	   
+	   //return the Car object
 	   return carObject;
    }
    
@@ -58,7 +70,7 @@ public class CarTester {
 		   }
 		      
 	   }
-	   catch(IOException e) {
+	   catch(IOException e) { //what to do if the file does not exist? tell the user
 		   System.out.println("THE file does not exist");
 	   }
 	   
