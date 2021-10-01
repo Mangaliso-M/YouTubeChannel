14c66a1c763e9a09abd84460a8628d3fa69ed7ca
Mangaliso Mngomezulu <65779966+Mangaliso-M@users.noreply.github.com>
2021-10-01 22:31:23 +0200

    Update Driver.java

--- object-txtFile/Driver.java
+++ object-txtFile/Driver.java
@@ -1,4 +1,8 @@
-package gettingBetter;
+/*
+  @Author: Mangaliso Mngomezulu
+  BSc Computer Science and Information Technology Student, University Of KwaZulu, Natal South Africa
+  Description: Driver class that represents a driver using two attributes, the name and the age of the driver.
+*/
 
 public class Driver {
 	
